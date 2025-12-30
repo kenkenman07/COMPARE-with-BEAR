ce2d2eea47806e4e01f1fc7243cfc889e003e20f
kimujin <kimura.hayato.23@shizuoka.ac.jp>
2025-12-30 16:57:39 +0900

    backend websocketに変更
    backend websocketに変更2

--- backend/app/api/routes.py
+++ backend/app/api/routes.py
@@ -1,28 +1,33 @@
-from fastapi import APIRouter
+import asyncio
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect
 from app.simulation.manager import manager
-from app.simulation.state import SimulationState
+from app.simulation.state import SimulationStatus
 
-router = APIRouter(prefix="/simulation", tags=["simulation"])
+router = APIRouter()
+# ユーザーが走った距離とユーザが走った時間を返す．
+@router.websocket("/simulation")
+async def start(websocket: WebSocket):
+    await websocket.accept()
 
+    params = websocket.query_params
+    time_50m = float(params["time_50m"])
+    distance = float(params["distance"])
 
-@router.get("/start")
-def start(human_max_speed: float, distance: float) -> SimulationState:
-    manager.start(human_max_speed, distance)
-    return manager.state
-
-
-@router.get("/pause")
-def pause() -> SimulationState:
-    manager.pause()
-    return manager.state
-
-
-@router.get("/reset")
-def reset() -> SimulationState:
-    manager.reset()
-    return manager.state
-
-
-@router.get("/state")
-def get_state() -> SimulationState:
-    return manager.state
+    manager.start(time_50m, distance)
+    
+    try:
+        while True:
+            await asyncio.sleep(0.1) 
+            # 終了判定
+            if manager.state.status == SimulationStatus.FINISHED:
+                # 最終結果を送信する
+                await websocket.send_json({
+                    "type": "result",
+                    "user_running_distance": round(manager.state.user.running_distance, 1),
+                    "total_time": round(manager.state.total_time, 1)
+                })
+                break
+    except WebSocketDisconnect:
+        manager.reset()
+          
+    
