aef469bedc06f5e6990b188777665ea313b7d8cd
kenkenman07 <ken0522g@icloud.com>
2026-01-11 13:15:44 +0900

    リファクタリング

--- frontend/src/components/ResultModal.tsx
+++ frontend/src/components/ResultModal.tsx
@@ -45,7 +45,7 @@ export function ResultModal() {
                             onClick={() => navigate("/data")}
                             className="flex justify-center py-3 px-5 mt-15 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-slate-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring--500 disabled:opacity-50 disabled:cursor-not-allowed"    
                         >
-                            もう一回競争する
+                            もう一度シミュレーション
                         </button>
                     </div>
                 </div>
