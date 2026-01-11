7f667f998d36efdded4cffacbd30d0bc1fde4d2d
kimujin <kimura.hayato.23@shizuoka.ac.jp>
2026-01-11 14:08:55 +0900

    ResultModalページ　デザイン修正

--- frontend/src/components/ResultModal.tsx
+++ frontend/src/components/ResultModal.tsx
@@ -29,23 +29,51 @@ export function ResultModal() {
                 transition-opacity duration-300 ease-out
                 ${isOpen ? "opacity-100" : "opacity-0 pointer-events-none"}                
             `}>
-                <div className="bg-white/70 p-6 w-[1000px] h-[600px] my-[100px] mx-auto rounded-lg">
-                    <div className="flex flex-col items-center text-center gap-4">
+                <div className="relative bg-white/70 p-6 w-[1000px] h-[600px] my-[100px] mx-auto rounded-lg">
+                    <div className="flex flex-col items-center text-center gap-4 ">
 
-                        <h1 className="text-7xl font-bold">追いつかれた！</h1>
+                        <img src="images/human_chased.png" className="absolute -top-40 w-00 " />
                         
-                        <div className="text-4xl mt-25">
-                            <h3 className="font-semibold">走った距離</h3>
-                            <div>{result!.user_distance}m</div>
-                            <h3 className="font-semibold mt-2">走った時間</h3>
-                            <div>{result!.time}秒</div>
+                        <div className="text-4xl mt-50">
+                            <h3 className="font-semibold">走行距離</h3>
+                            <span className="font-bold text-6xl">{result!.user_distance}</span>m
+                            <h3 className="font-semibold mt-5">走行時間</h3>
+                            <span className="font-bold text-6xl">{result!.time}</span>秒
                         </div>
                         
                         <button 
                             onClick={() => navigate("/data")}
-                            className="flex justify-center py-3 px-5 mt-15 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-slate-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring--500 disabled:opacity-50 disabled:cursor-not-allowed"    
+                            className="bg-transparent
+                                flex justify-center 
+                                py-3 
+                                px-5 
+                                mt-8
+                                focus:outline-none 
+                                focus:ring-2 
+                                focus:ring-offset-2 
+                                focus:ring--500 
+                                text-amber-100
+                                border-[3px] border-amber-600
+                                text-[18px]
+                                rounded-lg
+                                cursor-pointer
+                                font-bold
+                                uppercase
+                                tracking-[3px]
+                                transition-all duration-300 ease-in-out
+                                shadow-[0_0_10px_rgba(251,191,36,0.8),0_0_20px_rgba(251,191,36,0.8),inset_0_0_10px_rgba(251,191,36,0.3)]
+                                hover:bg-amber-600
+                                hover:text-white
+                                hover:shadow-[0_0_20px_rgba(251,191,36,1),0_0_40px_rgba(251,191,36,1),0_0_60px_rgba(251,191,36,1),inset_0_0_20px_rgba(255,255,255,0.3)]
+                                hover:[text-shadow:0_0_10px_#fff]
+                                active:scale-95
+                                disabled:opacity-70
+                                disabled:cursor-not-allowed
+                                disabled:hover:bg-transparent
+                                disabled:hover:text-amber-600
+                                disabled:hover:shadow-none"
                         >
-                            もう一度シミュレーション
+                            もう一度 シミュレーションへ
                         </button>
                     </div>
                 </div>
